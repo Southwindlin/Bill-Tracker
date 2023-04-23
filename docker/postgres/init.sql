faaea3cbd46011b06f95b0ce952ba2e410131593
SouthwindLin <southwind0523@gmail.com>
2023-04-23 00:30:01 -0400

    initialized the project

---
+++ docker/postgres/init.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE billsplitter;
+
+\c billsplitter;
+
+CREATE TABLE users (
+                       id SERIAL PRIMARY KEY,
+                       name VARCHAR(255) NOT NULL,
+                       email VARCHAR(255) UNIQUE NOT NULL
+);
+
+CREATE TABLE bills (
+                       id SERIAL PRIMARY KEY,
+                       amount NUMERIC(10, 2) NOT NULL,
+                       category VARCHAR(255) NOT NULL,
+                       purpose VARCHAR(255) NOT NULL,
+                       date DATE NOT NULL,
+                       payer INTEGER REFERENCES users(id)
+);
